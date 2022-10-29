313d7241e2a25a090a5c23156198dda1ea89066c
unknown <mehmoodhassan595@yahoo.com>
2022-10-29 15:50:45 +0500

    Profile and essentials created

--- main.py
+++ main.py
@@ -8,17 +8,34 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
+
+@app.route("/add_route", methods = ['POST'])
+def add_route():
+    data = request.get_json()
+    user_management.add_route(data)
+    return {"message":"Route added successfully"}
+
+
+@app.route("/all_routes")
+def all_route():
+    routes = user_management.get_all_routes()
+    return jsonify(routes)
+
+
+@app.route("/delete_route", methods = ['POST'])
+def delete():
+    data = request.get_json()
+    user_management.delete_route(data)
+    return {"message":"Route added successfully"}
 
 
 @app.route("/pessengerLogin", methods = ['POST'])
 def pessenger_login():
+
     login_credentials = request.get_json()
+
     login = user_management.pessanger_login(login_credentials)
     if login:
         return jsonify(login)
@@ -36,12 +53,13 @@ def pessenger_signUp():
 
 @app.route("/verify_pessenger/<string:session_id>")
 def verify_franchise(session_id):
-    pessenger_details = user_management.pessanger_login(session_id)
+    pessenger_details = user_management.verify_session_session(session_id)
     if pessenger_details:
         return jsonify(pessenger_details)
     
     return jsonify({"message":"User not authorized for this request"})
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__=='__main__':
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
--- user_management.py
+++ user_management.py
@@ -5,31 +5,32 @@ import uuid
 
 #Connection with Database
 try:
-    mongo = flask_pymongo.MongoClient("mongodb://localhost:27017/")
-    db = mongo.fast
+    client = flask_pymongo.MongoClient("mongodb://my_team:<password>@ac-ga3haug-shard-00-00.59spobs.mongodb.net:27017,ac-ga3haug-shard-00-01.59spobs.mongodb.net:27017,ac-ga3haug-shard-00-02.59spobs.mongodb.net:27017/?ssl=true&replicaSet=atlas-ugvkvk-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.fast
+
     
 
     
-    mongo.server_info()
+    client.server_info()
 except:
     print("Error cannot connect to db")
 
 
 
 def pessanger_login(login_credentials):
-    pessenger = db.pessengers.find({"email":login_credentials['email'],'password':login_credentials['password']})
+    pessenger = db.pessengers.find({"email":login_credentials['email'],'pass':login_credentials['pass']})
     try:
         pessenger = parse_json(pessenger[0])
         session = dict(pessenger['_id'])['$oid']+str(uuid.uuid4())
-        db.sessions.insert_one({'session_id':session,'user_previliges':'pessenger',"email": pessenger["franchise_id"]})
+        db.sessions.insert_one({'session_id':session,'user_previliges':'pessenger',"email": pessenger["email"]})
         pessenger['session_id'] = session
         return pessenger
     except IndexError:
         return False
 
-def pessanger_signup(pessenger_details):
-    pessenger_details["history"] = []
-    db.pessengers.insert_one(pessenger_details)
+def pessanger_signup(franchise_details):
+    franchise_details["history"] = []
+    db.pessengers.insert_one(franchise_details)
     return True
 
 
@@ -38,11 +39,29 @@ def verify_session_session(session_id):
     try:
         session = parse_json(sessions[0])
         if session['user_previliges'] == 'pessenger':
-            return True
+            user = parse_json(db.pessengers.find({'email':session['email']})[0])
+            return user
         return False
 
     except IndexError:
         return False
+
+
+
+def add_route(route_details):
+    db.routes.insert_one(route_details)
+    return True
+
+
+
+def get_all_routes():
+    routes = db.routes.find({})
+    return parse_json(routes)
+
+
+def delete_route(route_details):
+    db.routes.delete_many(route_details)
+    return True
 
 
 def parse_json(data):
