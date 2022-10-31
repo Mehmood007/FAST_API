96d13b3f9edd0e5bd5ce6f2fdde2473f076409ce
unknown <mehmoodhassan595@yahoo.com>
2022-11-01 00:05:26 +0500

    Basic registration routes completed

--- main.py
+++ main.py
@@ -8,28 +8,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def testing():
-    return jsonify({"message":"Working fine"})
 
-@app.route("/add_route", methods = ['POST'])
-def add_route():
-    data = request.get_json()
-    user_management.add_route(data)
-    return {"message":"Route added successfully"}
+#Pessenger registration and verifications routes are below
+@app.route("/pessenger_signup", methods = ['POST'])
+def pessenger_signUp():
+    franchise_details = request.get_json()
+    signup = user_management.pessanger_signup(franchise_details)
+    if signup:
+        return jsonify({"message":"User Signed UP successfully"})
+    return jsonify({"message":"Given email is already registered"})
 
-
-@app.route("/all_routes")
-def all_route():
-    routes = user_management.get_all_routes()
-    return jsonify(routes)
-
-
-@app.route("/delete_route", methods = ['POST'])
-def delete():
-    data = request.get_json()
-    user_management.delete_route(data)
-    return {"message":"Route added successfully"}
 
 
 @app.route("/pessengerLogin", methods = ['POST'])
@@ -43,22 +31,99 @@ def pessenger_login():
     return jsonify({"message":"Error in signing in to pessenger"})
 
 
-@app.route("/pessenger_signup", methods = ['POST'])
-def pessenger_signUp():
-    franchise_details = request.get_json()
-    signup = user_management.pessanger_signup(franchise_details)
-    if signup:
-        return jsonify({"message":"User Signed UP successfully"})
-    return jsonify({"message":"Error in signing up pessenger"})
-
 
 @app.route("/verify_pessenger/<string:session_id>")
-def verify_franchise(session_id):
-    pessenger_details = user_management.verify_session_session(session_id)
+def verify_pessenger(session_id):
+    pessenger_details = user_management.verify_pessenger_session(session_id)
     if pessenger_details:
         return jsonify(pessenger_details)
     
     return jsonify({"message":"User not authorized for this request"})
+
+
+
+#Driver registration and verification routes are below
+
+@app.route("/driver_signup", methods = ['POST'])
+def driver_signUp():
+    franchise_details = request.get_json()
+    signup = user_management.driver_signup(franchise_details)
+    if signup:
+        return jsonify({"message":"User Signed UP successfully"})
+    return jsonify({"message":"Given email already in registered"})
+
+
+@app.route("/driverLogin", methods = ['POST'])
+def driver_login():
+
+    login_credentials = request.get_json()
+
+    login = user_management.driver_login(login_credentials)
+    if login:
+        return jsonify(login)
+    return jsonify({"message":"Error in signing in to pessenger"})
+
+
+
+@app.route("/verify_driver/<string:session_id>")
+def verify_driver(session_id):
+    pessenger_details = user_management.verify_driver_session(session_id)
+    if pessenger_details:
+        return jsonify(pessenger_details)
+    
+    return jsonify({"message":"User not authorized for this request"})
+
+
+
+#Routes related routes
+#Driver routes are given below
+@app.route("/add_route", methods = ['POST'])
+def add_route():
+    session_id = request.headers['session_id']
+    driver = user_management.verify_driver_session(session_id)
+    if driver:
+        data = request.get_json()
+        user_management.add_route(data,driver["email"])
+        return jsonify({"message":"Route added successfully"})
+    
+    return jsonify({"message":"User is not authorized to add this route"})
+    
+
+
+
+@app.route("/delete_route", methods = ['POST'])
+def delete_route():
+    session_id = request.headers['session_id']
+    driver = user_management.verify_driver_session(session_id)
+    if driver:
+        data = request.get_json()
+        data["route_driver"] = driver["email"]
+        user_management.delete_route(data)
+        return {"message":"Route deleted successfully"}
+
+    return jsonify({"message":"User is not authorized to delete route"})
+
+
+#Pessenger related routes
+@app.route("/all_routes")
+def all_route():
+    routes = user_management.get_all_routes()
+    return jsonify(routes)
+
+
+@app.route("/select_route",methods=["POST"])
+def select_routes():
+    session_id = request.headers['session_id']
+    pessenger = user_management.verify_pessenger_session(session_id)
+    if pessenger:
+        route = request.get_json()["route"]
+        seats = request.get_json()["seats"]
+        user_management.select_route(pessenger["email"],route,seats)
+        return jsonify({"message":"Route selected successfully"})
+
+    return jsonify({"message":"User is not authorized"})
+
+
 
 
 if __name__=='__main__':
--- user_management.py
+++ user_management.py
@@ -17,8 +17,18 @@ except:
 
 
 
+#Below funcitons are related to pessenger
+def pessanger_signup(franchise_details):
+    similar_emails = parse_json(db.pessengers.find({"email":franchise_details["email"]}))
+    if similar_emails:
+        return False
+    franchise_details["history"] = []
+    db.pessengers.insert_one(franchise_details)
+    return True
+
+
 def pessanger_login(login_credentials):
-    pessenger = db.pessengers.find({"email":login_credentials['email'],'pass':login_credentials['pass']})
+    pessenger = db.pessengers.find({"email":login_credentials['email'],'password':login_credentials['password']})
     try:
         pessenger = parse_json(pessenger[0])
         session = dict(pessenger['_id'])['$oid']+str(uuid.uuid4())
@@ -28,13 +38,9 @@ def pessanger_login(login_credentials):
     except IndexError:
         return False
 
-def pessanger_signup(franchise_details):
-    franchise_details["history"] = []
-    db.pessengers.insert_one(franchise_details)
-    return True
 
 
-def verify_session_session(session_id):
+def verify_pessenger_session(session_id):
     sessions = db.sessions.find({'session_id':session_id})
     try:
         session = parse_json(sessions[0])
@@ -48,20 +54,81 @@ def verify_session_session(session_id):
 
 
 
-def add_route(route_details):
-    db.routes.insert_one(route_details)
+#Below are drivers routes
+def driver_signup(franchise_details):
+    similar_emails = parse_json(db.drivers.find({"email":franchise_details["email"]}))
+    if similar_emails:
+        return False
+    franchise_details["history"] = []
+    db.drivers.insert_one(franchise_details)
     return True
 
 
+def driver_login(login_credentials):
+    pessenger = db.drivers.find({"email":login_credentials['email'],'password':login_credentials['password']})
+    try:
+        pessenger = parse_json(pessenger[0])
+        session = dict(pessenger['_id'])['$oid']+str(uuid.uuid4())
+        db.sessions.insert_one({'session_id':session,'user_previliges':'driver',"email": pessenger["email"]})
+        pessenger['session_id'] = session
+        return pessenger
+    except IndexError:
+        return False
 
-def get_all_routes():
-    routes = db.routes.find({})
-    return parse_json(routes)
+
+
+def verify_driver_session(session_id):
+    sessions = db.sessions.find({'session_id':session_id})
+    try:
+        session = parse_json(sessions[0])
+        if session['user_previliges'] == 'driver':
+            user = parse_json(db.drivers.find({'email':session['email']})[0])
+            return user
+        return False
+
+    except IndexError:
+        return False
+
+
+
+
+#Below are routes functions
+#Driver's related routes
+def add_route(route_details,drivers_mail):
+    driver_history = parse_json(db.drivers.find({"email":drivers_mail})[0]['history'])
+    driver_history.append(route_details)
+    db.drivers.find_one_and_update({'email': drivers_mail},{'$set':{'history': driver_history}})
+    route_details["route_driver"] = drivers_mail
+    route_details["bookings"] = []
+    route_details["seats"] = [False for i in range(15)]
+    db.routes.insert_one(route_details)
+    return True
 
 
 def delete_route(route_details):
     db.routes.delete_many(route_details)
     return True
+
+#Pessenger's related routes
+def get_all_routes():
+    routes = db.routes.find({})
+    return parse_json(routes)
+
+
+def select_route(pessenger_mail,route,seats):
+    route_details = parse_json(db.routes.find({route})[0])
+    print(route_details)
+    for i in seats:
+        route_details["seats"][i]=True
+
+    route_details["bookings"].append(pessenger_mail)
+    print("\n\n\nWorking here\n\n\n")
+    db.routes.find_one_and_update({"_id":dict(route['_id'])['$oid']},{'$set':route_details})
+
+    return True
+
+    
+    
 
 
 def parse_json(data):
